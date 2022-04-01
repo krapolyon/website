6c6497beaa818488c0788c37ae908768d3549b48
Nicolas Vuaille <nicolas.vuaille@gmail.com>
2022-04-01 15:18:15 +0200

    Ajout du tag meta viewport

--- Partoches/random.php
+++ Partoches/random.php
@@ -1,6 +1,7 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
   <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>
       Al&eacute;atoire : Adj, Qui se produit par hasard.
     </title>
